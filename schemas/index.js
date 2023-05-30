013eab8291df7c5269fa718d995643466235a182
Allan Fernandes <Fernzallan14@gmail.com>
2023-05-30 14:08:03 +0100

    Pizza schema

--- schemas/index.js
+++ schemas/index.js
@@ -2,5 +2,5 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
-
-export const schemaTypes = [post, author, category, blockContent]
+import pizza from './pizza'
+export const schemaTypes = [post, author, category,pizza, blockContent]
--- schemas/pizza.js
+++ schemas/pizza.js
@@ -1,6 +1,40 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-    name: 'pizza',
-    title: 'Pi'
+  name: 'pizza',
+  title: 'Pizzas',
+  icon: () => "🍕",
+  type: 'document',
+  fields: [
+   defineField({
+    name: 'name',
+    title: 'Pizza name',
+    type: 'string',
+    description: 'Name of the pizza'
+   }),
+   defineField({
+    name: 'slug',
+    title: 'Slug',
+    type: 'slug',
+    options: {
+        source: 'name',
+        maxLength: 96
+    }
+   }),
+   defineField({
+    name: 'image',
+    title: 'Image',
+    type: 'image',
+    options: {
+        hotspot: true,
+    }
+   }),
+   defineField({
+    name: 'price',
+    title: 'Price',
+    type: 'number',
+    description: 'Price of the Pizza in cents',
+    validation: Rule => Rule.min(1000).max(5000),
+   })
+  ],
 })
